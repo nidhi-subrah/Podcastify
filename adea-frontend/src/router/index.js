1af845878ff4ff3e0200b5e302d22d34e3427edd
Michelle Dominic <michelle.m.dominic@gmail.com>
2025-01-25 14:22:57 -0500

    Frontend with chatbot and looks

--- adea-frontend/src/router/index.js
+++ adea-frontend/src/router/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
-
 import Home from "../views/Home.vue";
 import Upload from "../views/Upload.vue";
 import Feedback from "../views/Feedback.vue";
+import Chatbot from "../components/Chatbot.vue"; // Import Chatbot component
 
 const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/upload", name: "Upload", component: Upload },
   { path: "/feedback", name: "Feedback", component: Feedback },
+  { path: "/chatbot", name: "Chatbot", component: Chatbot }, // Chatbot route
 ];
 
 const router = createRouter({
