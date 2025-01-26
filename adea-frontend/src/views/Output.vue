0f6d49845e9681336855a9f74ef1e464810c3af0
Michelle Dominic <michelle.m.dominic@gmail.com>
2025-01-26 02:12:18 -0500

    Fixing the audioplayer path

--- adea-frontend/src/views/Output.vue
+++ adea-frontend/src/views/Output.vue
@@ -4,15 +4,27 @@
     <p>Your text has been converted into a podcast! Click play to listen.</p>
 
     <!-- Pass the audio file URL as a prop -->
-    <AudioPlayer audioUrl="http://localhost:5000/uploads/podcast.mp3" />
+    <AudioPlayer :audioUrl="audioUrl" />
   </div>
 </template>
 
 <script>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
 import AudioPlayer from "../components/AudioPlayer.vue";
 
 export default {
   components: { AudioPlayer },
+  setup() {
+    const route = useRoute();
+    const audioUrl = ref("");
+
+    onMounted(() => {
+      audioUrl.value = route.query.audioUrl;
+    });
+
+    return { audioUrl };
+  },
 };
 </script>
 
